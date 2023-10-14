b60fbfa913d0d00ad9fd425d8c568a0a8c883f89
Salmonbear <si_bruce@hotmail.com>
2023-10-14 21:53:06 +0100

    Update _app.js
    copy and paste of the app.js file that worked for last hecto repo

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,24 @@
-import Script from 'next/script'
- 
-export default function MyApp({ Component, pageProps }) {
+import '../styles/globals.css'
+import { PlasmicRootProvider } from "@plasmicapp/react-web";
+import Head from "next/head";
+import Script from "next/script";
+
+function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <><PlasmicRootProvider Head={Head}>
       <Component {...pageProps} />
-      <script src="https://beamanalytics.b-cdn.net/beam.min.js" data-token="f76cd8af-c1f3-4945-8694-b3733e439047" async>
+    </PlasmicRootProvider><Script
+      id="beam-analytics"
+      strategy="afterInteractive"
+      src="https://beamanalytics.b-cdn.net/beam.min.js"
+      data-token="f76cd8af-c1f3-4945-8694-b3733e439047"
+      async>
+    </Script><Script
+      id="plausible"
+      defer data-domain="hecto.io"
+      src="https://plausible.io/js/script.js">
+      </Script>
     </>
-  )
+  );
 }
+export default MyApp
